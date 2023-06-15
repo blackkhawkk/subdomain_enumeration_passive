0c9167562a8648c2300b0a5b4183ff9e5664c983
Tanmay Bhattacharjee <60282451+blackkhawkk@users.noreply.github.com>
2023-06-16 03:50:44 +0530

    Update buffer.sh

--- passive_scan/buffer.sh
+++ passive_scan/buffer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 domain=$1
-curl "https://tls.bufferover.run/dns?q=$domain" -H 'x-api-key: ls9sK55VKMaRzB96FxV4J7VwIDn0VpdV13FvPp0p' | jq -r '.Results[]' | cut -d ',' -f5 | grep -F "$domain" | sort -u
+curl "https://tls.bufferover.run/dns?q=$domain" -H 'x-api-key: ' | jq -r '.Results[]' | cut -d ',' -f5 | grep -F "$domain" | sort -u
