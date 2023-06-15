8f4f48638a7085bee58b9497ddcc93857d0c4564
Tanmay Bhattacharjee <60282451+blackkhawkk@users.noreply.github.com>
2023-06-16 03:51:30 +0530

    Update security_trail.sh

--- passive_scan/security_trail.sh
+++ passive_scan/security_trail.sh
@@ -2,4 +2,4 @@
 
 domain=$1
 
-curl --silent --insecure "https://api.securitytrails.com/v1/domain/$1/subdomains?children_only=false&include_inactive=false" --header 'APIKEY: LTmzSKGTo3jDMmzK3WjeQpdS53qVHwgD' | jq -r '.subdomains[]' | awk '{sub(/^\./, ""); print $0 "." "'$1'"}' | sort -u
+curl --silent --insecure "https://api.securitytrails.com/v1/domain/$1/subdomains?children_only=false&include_inactive=false" --header 'APIKEY: ' | jq -r '.subdomains[]' | awk '{sub(/^\./, ""); print $0 "." "'$1'"}' | sort -u
